f378c0f0b74e4117ae8172c6cb68399539535aca
강대은 <eodms4334@naver.com>
2023-08-26 20:26:37 +0900

    java 여러 알고리즘 풀이 add

---
+++ algorism_java/src/algorism_java/궁금해서푸는공간.java
@@ -0,0 +1,17 @@
+package algorism_java;
+
+public class 궁금해서푸는공간 {
+
+	public static void main(String[] args) {
+		double a = 1.55;
+		double b = 1.33;
+		
+		System.out.printf("%.0f\n", a);
+		System.out.printf("%.1f\n", a);
+		
+		System.out.printf("%.0f\n", b);
+		System.out.printf("%.1f\n", b);
+
+	}
+
+}
