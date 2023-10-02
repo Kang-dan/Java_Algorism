a313a96580aa7e629b6d8be0aebb88801279564e
강대은 <eodms4334@naver.com>
2023-10-02 23:08:32 +0900

    Programmers 알고리즘 풀이

--- algorism_java/src/스터디/PG_DFS_여행경로.java
+++ algorism_java/src/스터디/PG_DFS_여행경로.java
@@ -1,3 +1,4 @@
+//DFS + 2차원배열 정렬 
 package 스터디;
 
 import java.util.*;
@@ -6,10 +7,11 @@ import java.io.*;
 public class PG_DFS_여행경로 {
 
 	class Solution {
-
+		
 	    static boolean[] visit;
 	    static String[] answer;
 	    public String[] solution(String[][] tickets) {
+	    		    	
 	        answer = new String[tickets.length + 1];
 	        visit = new boolean[tickets.length + 1];
 
@@ -25,6 +27,17 @@ public class PG_DFS_여행경로 {
 	            }
 	            return 0;
 	        }
+	    });
+	    
+	    Arrays.sort(tickets, new Comparator<String[]>() {
+
+			@Override
+			public int compare(String[] o1, String[] o2) {
+				if(o1[0].equals(o2[0])) return o1[1].compareTo(o2[1]);
+				else if(!o1[0].equals(o2[0])) return o1[0].compareTo(o2[0]);
+				return 0;
+			}
+	    	
 	    });
 
 	    for(int i = 0; i < tickets.length; i++) {
@@ -61,3 +74,37 @@ public class PG_DFS_여행경로 {
 	    }
 	}
 }
+
+/*
+Arrays.sort(tickets, new Comparator<String[]>() {
+    @Override
+    public int compare(String[] o1, String[] o2) {
+        if(o1[0].equals(o2[0])) {
+            return o1[1].compareTo(o2[1]);
+        }
+        else if(!o1[0].equals(o2[0])) {
+            return o1[0].compareTo(o2[0]);
+        }
+        return 0;
+        
+        /*
+        
+        - compareTo 메서드가 같으면 0을 return 하기 때문에  따로 처리해 줄 필요 없음
+        
+        if(o1[0].equals(o2[0])) {
+            return o1[1].compareTo(o2[1]);
+        }
+        else {
+            return o1[0].compareTo(o2[0]);
+        } 
+        
+        - 더 간략하게 하려면 중괄호 생략하고 한줄 씩
+        
+        if(o1[0].equals(o2[0])) return o1[1].compareTo(o2[1]);
+        else return o1[0].compareTo(o2[0]);
+        
+          - 람다식으로 한 경우
+          
+      Arrays.sort(tickets, (o1, o2) -> o1[0].equals(o2[0]) ? o1[1].compareTo(o2[1]) : o1[0].compareTo(o2[1]);
+       
+        */
