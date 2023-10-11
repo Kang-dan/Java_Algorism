1dab48023e6843708df307cbc99b02c00caf355a
강대은 <eodms4334@naver.com>
2023-10-12 07:48:01 +0900

    BJ 스도쿠

--- algorism_java/src/algorism_java/BJ_2239_스도쿠_순열로풀어보자.java
+++ algorism_java/src/algorism_java/BJ_2239_스도쿠_순열로풀어보자.java
@@ -33,13 +33,14 @@ public class BJ_2239_스도쿠_순열로풀어보자 {
 			}
 			int c = 0;
 			input = new int[9-numCnt]; //없는 숫자 담을 배열 
+			Arrays.fill(input, 0);
 			for(int j = 0; j < 9; j++) {
 				if(!nonNum[j]) input[c++] = j+1;
 			}
 			Arrays.sort(input);
 			select = new boolean[10];
 			newInput = new int[input.length];
-			순열(input, 0, i);
+			순열(newInput, input, 0, i, select);
 		}
 		
 		//출력
@@ -58,18 +59,27 @@ public class BJ_2239_스도쿠_순열로풀어보자 {
 	static boolean[] select;
 	static int[] newInput;
 	
-	private static void 순열(int[] input, int cnt, int r) {
+	private static void 순열(int[] newInput, int[] input, int cnt, int r, boolean[] select) {
 		//가로줄 완성될 때마다 -> 세로줄, 3x3확인하기 
 		if(cnt == input.length) {
 			//세로줄 확인하러가기 
 			//3X3확인하러가기 
+//			if(r==0) {
+//				System.out.println(Arrays.toString(newInput));
+//			}
 			if(sero(newInput, r) && check3(map, newInput, r)) {
 				int idx = 0;
 				for(int j = 0; j < 9; j++) {
 					if(map[r][j] == 0) map[r][j] = newInput[idx++];
 				}
-				newInput = new int[input.length];
+				
+//				if(r == 0) {
+//					System.out.println(Arrays.toString(newInput));
+//					System.out.println(Arrays.toString(input));
+//					System.out.println();					
+//				}
 				return;
+//				newInput = new int[input.length];
 			}
 			return;
 		}
@@ -78,7 +88,7 @@ public class BJ_2239_스도쿠_순열로풀어보자 {
 			if(select[i]) continue;
 			select[i] = true;
 			newInput[cnt] = input[i];
-			순열(input, cnt+1, r);
+			순열(newInput, input, cnt+1, r, select);
 			select[i] = false;
 		}
 	}
@@ -93,6 +103,16 @@ public class BJ_2239_스도쿠_순열로풀어보자 {
 				map2[r][i] = newInput2[cnt++];
 			}
 		}
+		if(r==0) {
+			System.out.println("map2, r = " + r + " : ");
+			for(int i = 0; i < 9; i++) {
+				for(int j = 0; j < 9; j++) {
+					System.out.print(map2[i][j]+" ");
+				}
+				System.out.println();
+			}
+		}
+		
 		check = new boolean[10];
 		for(int i = y; i < y+3; i++) {
 			for(int j = 0; j < 3; j++) {
@@ -118,9 +138,13 @@ public class BJ_2239_스도쿠_순열로풀어보자 {
 	}
 	
 	private static boolean sero(int[] newInput2, int r) {
+		if(r==0) {
+			System.out.println(r+"");
+			System.out.println(Arrays.toString(newInput2));
+		}
 		int cnt = 0;
 		for(int i = 0; i < 9; i++) {
-			if(map[r][i]==0) {
+			if(map[r][i] == 0) {
 				int v = newInput2[cnt++];
 				for(int j = 0; j < 9; j++) {
 					if(map[j][i] == v) {
@@ -129,6 +153,11 @@ public class BJ_2239_스도쿠_순열로풀어보자 {
 				}
 			}
 		}
+		
+		if(r==0) {
+			System.out.println(Arrays.toString(newInput2));
+		}
+		System.out.println();
 		return true;
 	}
 
